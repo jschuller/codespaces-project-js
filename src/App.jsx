5f88f9895addbd04edc58d681046a0b2750f9d72
Josh Schuller <826699+jschuller@users.noreply.github.com>
2024-03-03 02:40:12 +0000

    initial commit; updating contact details.  let's gogit add .

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Josh Schuller",
+  title: "Cloud Architect",
+  email: "joshua_schuller@yahoo.com",
+  gitHub: "https://github.com/jschuller",
+  servicenow: "https://nowlearning.servicenow.com/lxp?id=nl_public&user=joshuaschuller",
+  linkedIn: "https://www.linkedin.com/in/joshuaschuller/",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  devdotto: "https://dev.to/jschuller",
 };
 
 const primaryColor = "#4E567E";
